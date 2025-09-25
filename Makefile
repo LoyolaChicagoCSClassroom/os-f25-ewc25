b882f4ff0cc0bc23e4e22a06f2ce7f8bcb33cc1b
ethan <ewcarr5@gmail.com>
2025-09-24 22:27:42 -0500

    homework 1

--- Makefile
+++ Makefile
@@ -1,8 +1,7 @@
-
 UNAME_M := $(shell uname -m)
 
 ifeq ($(UNAME_M),aarch64)
-PREFIX:=i686-linux-gnu-
+PREFIX:=i386-unknown-elf-
 BOOTIMG:=/usr/local/grub/lib/grub/i386-pc/boot.img
 GRUBLOC:=/usr/local/grub/bin/
 else
@@ -24,6 +23,7 @@ SDIR = src
 
 OBJS = \
 	kernel_main.o \
+        rprintf.o \
 
 # Make sure to keep a blank line here after OBJS list
 
@@ -48,8 +48,8 @@ obj:
 rootfs.img:
 	dd if=/dev/zero of=rootfs.img bs=1M count=32
 	$(GRUBLOC)grub-mkimage -p "(hd0,msdos1)/boot" -o grub.img -O i386-pc normal biosdisk multiboot multiboot2 configfile fat exfat part_msdos
-	dd if=/usr/local/grub/lib/grub/i386-pc/boot.img  of=rootfs.img conv=notrunc
 	dd if=$(BOOTIMG) of=rootfs.img conv=notrunc
+	dd if=grub.img of=rootfs.img conv=notrunc bs=512 seek=1 #########
 	echo 'start=2048, type=83, bootable' | sfdisk rootfs.img
 	mkfs.vfat --offset 2048 -F16 rootfs.img
 	mcopy -i rootfs.img@@1M kernel ::/
@@ -66,3 +66,4 @@ debug:
 
 clean:
 	rm -f grub.img kernel rootfs.img obj/*
+
--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,5 +1,6 @@
 
 #include <stdint.h>
+#include "rprintf.h"
 
 #define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
 
@@ -10,16 +11,56 @@ uint8_t inb (uint16_t _port) {
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
     return rv;
 }
+#define WIDTH 80
+#define HEIGHT 25
 
-void main() {
-    unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
-    const unsigned char color = 7; // gray text on black background
+struct termbuf{
+ char ascii;
+ char color;
+};
 
-    while(1) {
-        uint8_t status = inb(0x64);
+int x = 0;
+int y = 0;
 
-        if(status & 1) {
-            uint8_t scancode = inb(0x60);
-        }
-    }
+void scroller(void) {
+   struct termbuf *vram = (struct termbuf*)0xB8000;
+   for ( int row = 1; row < HEIGHT; row++){
+      for (int col = 0; col < WIDTH; col++){
+          vram[(row - 1) * WIDTH + col] = vram[row * WIDTH + col];
+      }
+   }
+  for (int col = 0; col < WIDTH; col++) {
+       vram[(HEIGHT -1) * WIDTH + col].ascii = ' ';
+       vram[(HEIGHT -1) * WIDTH + col].color = 7;
+   }
+ y = HEIGHT -1; 
+ x = 0;
+}
+static void print_char(char c){
+ struct termbuf *vram = (struct termbuf*)0xB8000;
+ if (c == '\n') {
+    x = 0;
+    y++;
+} else {
+   vram[y * WIDTH + x].ascii = c;
+   vram[y * WIDTH + x].color = 7;
+   x++;
+   if (x >= WIDTH){
+    x = 0;
+    y++;
+  }
+}
+ if (y >= HEIGHT){
+   scroller();
+  }
+}
+static int kputc(int c) {
+  print_char((char)c);
+  return c;
+}
+void main(void) {
+ for (int i = 0; i < 27; i++) {
+    esp_printf(kputc, "Ethan\n", i);
+ }
+ while(1);
 }
